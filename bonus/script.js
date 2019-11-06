8f13de42597eb87bc30a8a05743ca6baaca344d8
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-11-06 15:45:44 +0100

    sistemati id

--- bonus/script.js
+++ bonus/script.js
@@ -2,13 +2,19 @@
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi,
 // gli altri diventano verdi (tutti i rimanennti)
 // Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
+// BONUS:
+// generare la distribuzione di quelli che diventano rossi al click, in modo casuale;
+// generare l’intera griglia in js, invece che scriverla noi a mano nel file html;
+// controllo sul click di un quadrato, se è stato già cliccato dò un feedback aggiuntivo, oltre il fatto che è colorato di già;
+// varie ed eventuali che vi possono venire in mente.
 
 for (var i = 0; i < 8; i++) {
-  document.getElementById('tabella').innerHTML += "<tr id="+ [i] +"></tr>";
+  document.getElementById('tabella').innerHTML += "<tr id=row"+ [i] +"></tr>";
   for (var j = 0; j < 8; j++) {
-    document.getElementById([i]).innerHTML += "<td></td>";    
+    document.getElementById("row"+[i]).innerHTML += "<td></td>";    
   }
 }
+
 $(document).ready(function(){
 
   var contatoreRosso = [];
